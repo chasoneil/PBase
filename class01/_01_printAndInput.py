827d3ea45ec71af35d23d1c3ffef0e2de06a4d4e
chasoneil <chason001@126.com>
2025-09-05 14:49:34 +0800

    补充笔记

--- class01/_01_printAndInput.py
+++ class01/_01_printAndInput.py
@@ -1,15 +1,24 @@
-# 1. python 的 print
-# 2. python 的用户输入 input
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 1. python 的输出print()
+# 2. python 的输入input()
 # 3. python 的数据类型
 # 4. 逻辑运算符
+
+# 1. print()
+# 正常输出
+print('Hello python.')
 
 # print 中使用逗号可以分别输出多个字符串 遇到,会补充一个空格
 print('Hello', 'Python', 'Over')   # result: Hello Python Over
 
+# 输出函数中可以包含运算
 print('1 + 2 =', 1+2)
 
-print('01----------------------')
+print('01----------------------\n')
 
+# 2. input()
 # 使用input获取用户输入，回车结束
 name = input()
 print('Hello', name)
@@ -18,26 +27,25 @@ print('Hello', name)
 name = input('Please input your name:')
 print('Hello', name)
 
-print('02----------------------')
+print('02----------------------\n')
 
+# 3. 数据类型
 # python的数据类型： 整数 浮点数 字符串 布尔 空值
 x = 10  # 整数
 print('x =', x)
 
-pi = 3.14
+pi = 3.14 # 浮点数
 print('pi = ', pi)
 
 # python 中使用 '' / "" / ''' '''/ 来表示字符串
 print('single str')
 print("double str")
+# 字符串分行
 print('''long
 str''')
 
 print('I\'m Chinese')  # 有些字符需要转义
-
 print(r'\\\\t1')  # 使用 r可以让转义失效
-
-print('03----------------------')
 
 # python 中的布尔值是 True False
 if True:
@@ -46,11 +54,13 @@ else:
     print(False)
 
 # python中使用None表示空值
+a = None
+print('a =', a)
 
-print('04----------------------')
+print('03----------------------\n')
 
-# python 中使用and or not来代表与或非
-
+# 4. python 中使用and or not来代表与或非
+print('使用and or not 做逻辑运算:')
 if True and True:
     print('double true')
 
@@ -61,3 +71,16 @@ if not True:
     print('not true')
 else:
     print('not false')
+
+print('使用非关键字的方式做逻辑运算:')
+condition = True & True
+if condition:
+    print('double true')
+else:
+    print('has false')
+
+condition = True | False
+if  condition:
+    print('has true')
+else:
+    print('double false')
--- class01/_02_string.py
+++ class01/_02_string.py
@@ -1,30 +1,69 @@
-# 1. String 字符串
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# String 字符串
+# 1. 字符串分割  slice
+# 2. 常见的字符串函数 len()
+# 3. 字符串输出格式化的两种方式
+# 4. 字符和unicode的转化 ord() chr()
+#    搞清楚编码中 ascii unicode utf8之间的关系
+# 5. 字符和字节的转换  encode() decode()
+
 
 # 字符串切割
 str = "0123456789"
-print(str[0:5]) # 切割从第0个开始到第5个结束的字符串
-print(str[5:]) # 切割从第5个开始到最后的字符串
-print(str[:5]) # 切割从第0个开始到第5个结束的字符串
-print(str[::2]) # 切割从第0个开始到最后的字符串，每隔2个取一个
-print(str[::-1]) # 切割从最后一个开始到第一个结束的字符串，步长为-1
 
-print('01------------------------')
+print('从0-5:', str[0:5]) # 切割从第0个开始到第5个结束的字符串
+print('可以省略最初的0:', str[:5])
+
+print('从5-end:', str[5:]) # 切割从第5个开始到最后的字符串
+
+print('0-end,每隔2个:', str[::2]) # 切割从第0个开始到最后的字符串，每隔2个取一个
+
+# 切割从最后一个开始到第一个结束的字符串，步长为-1
+# 步长-1表示全量保存
+print('0-end,步长为-1:', str[::-1])
+
+print('01------------------------\n')
 
 # 常见的字符串函数
 name = 'chason'
 print('name length:', len(name)) # 获取字符串长度
 
-print('02------------------------')
+print('02------------------------\n')
 
-# 字符串格式化: 使用 % 进行指定拼接，如果有多个数据，需要用()括起来
+# 字符串格式化:
+# 方法1：使用 % 进行指定拼接，如果有多个数据，需要用()括起来
+
+print('%s %s' %('Hello', 'python'))
 print('Hello, My name is %s, my score is: %d' % ('chason', 100))
-
 print('My name is: %s' % 'fox')
 
+# 对浮点类型的格式化做要求
 print('%.2f' % 3.1415)  # result: 3.14
 
-print('03------------------------')
-
-# 字符串的第二种的格式化 format()
-
+# 方法二: format()
 print('Hello, My name is:{0}, my score is: {1:.2f}'.format('chason', 99.8887))
+
+print('03------------------------\n')
+
+# 4. 字符和 unicode之间的转化
+# 使用 chr() 实现 unicode -> 字符
+# 使用 ord() 实现 字符 -> 整数
+
+a = '中'
+print('\'中\'的unicode:', ord(a))
+
+b = 'x'
+print('x的unicode:', ord(b))
+
+print('20013的字符:', chr(20013))
+print('120的字符:', chr(120))
+
+print('04------------------------\n')
+
+# 5. 字符和字节的转化 encode() decode()
+# 注意byte类型需要以b开头 b'xxxx'
+str = '中文'
+print('中文->bytes:', str.encode('utf-8'))
+print('解码成中文:', b'\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8'))
