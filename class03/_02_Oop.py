21750c27925f14925524c1556b70344e6c9f4ce2
chasoneil <chason001@126.com>
2025-09-08 17:38:15 +0800

    update notes 8th Sep

--- class03/_02_Oop.py
+++ class03/_02_Oop.py
@@ -4,7 +4,8 @@
 # 面向对象编程，概念和Java类似，通过class定义类
 
 # class + 类名(继承自哪个类)
-print('1. 创建一个普通类和实例化:')
+print('1. 类和对象:')
+print('1.1 创建一个普通类和实例化:')
 class Student(object):
 
     # 类似Java的构造方法
@@ -18,7 +19,7 @@ print(s)  # <__main__.Student object at 0x00000122282170E0>
 
 
 # 类包含属性
-print('2. 创建带参数的类:')
+print('1.2. 创建带参数的类:')
 class Student1(object):
 
     def __init__(self, name, score):
@@ -33,15 +34,21 @@ s1 = Student1('chason', 90)
 s1.print_msg()
 
 # 可以动态给对象增加属性
-print('3. 动态给类增加属性')
+print('1.3. 动态给对象增加属性')
 s1.age = 18
 print(s1.age)
 
-print('02 ------------------------')
+s2 = Student1('lily', 30)
+# print(s2.age)  s2 并没有age属性所以报错 所以只能动态给对象增加属性
+
+print('01 ------------------------\n')
+
 
 # 访问限制
 # 类似 Java 的 private 类型的变量，不能直接被外界访问，需要提供get set方法
 # python 中通过 __ 双下划线的形式实现private
+print('2. 访问限制:')
+print('2.1 通过__来定义private:')
 class Student2(object):
 
     def __init__(self, name, score):
@@ -58,10 +65,13 @@ fox = Student2('fox', 80)
 # print(fox.__name) 报错，获取不到
 print(fox.get_name())
 
-print('03 ------------------------')
+# 使用 _开头的类的变量是不推荐直接访问的变量，虽然这个变量可以被直接访问
+
+print('02 ------------------------\n')
 
 # 继承和多态是面向对象的三大特性之二
-
+print('3. 继承和多态:')
+print('3.1 继承:')
 class Animal(object):
     def run(self):
         print('Animal is running.')
@@ -69,9 +79,11 @@ class Animal(object):
 # 定义的Dog继承自Animal 和Java中的extend没啥两样
 class Dog(Animal):
 
+    # 子类可以有其他的扩展方法
     def eat(self):
         print('Dog eat bones.')
 
+    # 子类可以重写父类的方法
     def run(self):
         print('dog run very fast!')
 
@@ -88,9 +100,7 @@ cat1 = Cat()
 # 子类可以重写父类的方法
 cat1.run()
 
-print('04 ------------------------')
 # 判断数据类型
-
 a = list()
 print(isinstance(a, list))
 print(isinstance(dog1, Dog))
@@ -98,10 +108,63 @@ print(isinstance(dog1, Dog))
 # 子类也是父类的类型
 print(isinstance(dog1, Animal))
 
-def run_again(Animal):
-    Animal.run()
-    Animal.run()
+# 当我们希望传入的类型是Animal， 参数首字母反而小写，方法中也是直接使用 animal
+# 这个姑且先当做一种默认的规则遵守 todo
+print('3.2 多态:')
+def run_again(animal):
+    animal.run()
+    animal.run()
 
 # 多态的作用
 run_again(dog1)
 run_again(cat1)
+
+print('03 ------------------------\n')
+
+# 判断对象类型 type()
+
+print('4. 对象类型:')
+print('4.1 判断对象类型:')
+
+print(type(12))
+print(type('abc'))
+
+class Person(object):
+
+    def __init__(self):
+        pass
+
+p = Person()
+print(type(p)) # <class '__main__.Person'>
+
+# isinstance()
+print('4.2 判断对象是否属于某种类型:')
+
+print(isinstance(p, Person))
+print(isinstance([1, 2], (list, tuple)))    # [1, 2] 是list或者tuple
+
+# 使用 dir()
+print('4.3 获取对象的所有变量和方法')
+print('p(Person)的所有变量和方法:', dir(p))
+
+print('04 ------------------------\n')
+
+# 类属性和对象属性
+# 类属性就是属于类的属性 使用类.属性名直接获取
+# 类属性可以被对象覆盖，但是类属性依旧保留
+
+print('5. 类属性:')
+class User:
+
+    name = 'zhangsan'
+
+    def __init__(self, age):
+        self.age = age
+
+user = User(18)
+print('user name:', user.name, 'age:', user.age)
+print('通过类获取name:', User.name)
+
+user.name = 'lisi'
+print('修改 user name:', user.name)
+print('修改 user name, 原来类的name:', User.name)
--- class04/_01_exception.py
+++ class04/_01_exception.py
@@ -1,6 +1,13 @@
-# pytho exception
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 错误、异常和调试
+# 1. try except finally
+# 2. 同时抓取多个异常
+#
 
 # 1. try-except block for specific exception
+print('1.1 使用try except抓取指定异常:')
 def demo1():
     try:
         x = 10/0
@@ -8,32 +15,47 @@ def demo1():
     except ZeroDivisionError:
         print("Error: division by zero")
 
-# demo1()
+demo1()
 
-# 2. catch all exception
+print('1.2 抓取多个异常:')
 def demo2():
     try:
         a = 10 / 0
         print(a)
-    except:  # do not recommend
+    except ZeroDivisionError as e:
+        print('0 不能作为除数.')
+    except ValueError as e:
+        print('发生 value error.')
+
+demo2()
+
+print('1.3 抓取所有异常')
+# 3. catch all exception
+def demo3():
+    try:
+        a = 10 / 0
+        print(a)
+    except BaseException as e:  # 所有的异常都来自 BaseException
         print("Error: your code caught exception")
 
-# demo2()
+demo3()
 
-# 3. try except finally
-def demo3():
+print('1.4 try catch finally')
+# 4. try except finally
+def demo4():
     try:
         x = 10 / 0
         print(x)
     except ZeroDivisionError:
         print("Error: division by zero")
     finally:
-        print("finally coding")
+        print("finally...")
 
-# demo3()
+demo4()
 
-# 4. try-except-else
-def demo4():
+# 5. try-except-else
+print('1.5 try except else')
+def demo5():
     try:
         print("do try")
     except:
@@ -41,14 +63,31 @@ def demo4():
     else:
         print("no except.")
 
-# demo4()
+demo5()
 
-# 5. throw exception
-def demo5(level):
+# 6. throw exception
+# python 中使用raise 关键字抛出异常
+print('1.6 抛出异常')
+def demo6(level):
     if level < 1:
         raise Exception("level is under 1!")
     print("level:", level)
 
-demo5(0)
+# demo6(0)
 
+# 7. python 内置的logging 模块可以打印错误
+print('1.7 使用logging收集异常信息')
+import logging
+
+def foo(s):
+    return 10 / int(s)
+
+def demo07():
+    try:
+        foo('0')
+    except Exception as e:
+        logging.exception(e)
+
+demo07()
+print('END') # 还是会被执行
 
