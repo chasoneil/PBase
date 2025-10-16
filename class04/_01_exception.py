d2f8f5b163da78d6bbe20a09559bb25327c403b3
chasoneil <chason001@126.com>
2025-10-16 17:59:20 +0800

    review python except

--- class04/_01_exception.py
+++ class04/_01_exception.py
@@ -2,12 +2,11 @@
 # -*- coding: utf-8 -*-
 
 # 错误、异常和调试
-# 1. try except finally
-# 2. 同时抓取多个异常
-#
+
+import logging
+logging.basicConfig(level=logging.INFO)
 
 # 1. try-except block for specific exception
-print('1.1 使用try except抓取指定异常:')
 def demo1():
     try:
         x = 10/0
@@ -15,9 +14,7 @@ def demo1():
     except ZeroDivisionError:
         print("Error: division by zero")
 
-demo1()
-
-print('1.2 抓取多个异常:')
+# 2. 收集多个异常
 def demo2():
     try:
         a = 10 / 0
@@ -27,9 +24,16 @@ def demo2():
     except ValueError as e:
         print('发生 value error.')
 
-demo2()
+# 收集多个异常的时候，如果直接写异常也是可以的，但是，不写 as e 就没法获取具体的异常信息
+def demo2_1():
+    try:
+        a = 10 / 0
+        print(a)
+    except ZeroDivisionError:
+        print('error 1')
+    except ValueError:
+        print('error 2')
 
-print('1.3 抓取所有异常')
 # 3. catch all exception
 def demo3():
     try:
@@ -38,46 +42,34 @@ def demo3():
     except BaseException as e:  # 所有的异常都来自 BaseException
         print("Error: your code caught exception")
 
-demo3()
-
-print('1.4 try catch finally')
 # 4. try except finally
 def demo4():
     try:
         x = 10 / 0
         print(x)
-    except ZeroDivisionError:
+    except ZeroDivisionError as e:
         print("Error: division by zero")
     finally:
         print("finally...")
 
-demo4()
-
 # 5. try-except-else
-print('1.5 try except else')
 def demo5():
     try:
         print("do try")
-    except:
+    except BaseException:
         print("caught except.")
     else:
         print("no except.")
 
-demo5()
-
-# 6. throw exception
+# 6. 抛出异常
 # python 中使用raise 关键字抛出异常
-print('1.6 抛出异常')
 def demo6(level):
     if level < 1:
         raise Exception("level is under 1!")
     print("level:", level)
 
-# demo6(0)
-
 # 7. python 内置的logging 模块可以打印错误
-print('1.7 使用logging收集异常信息')
-import logging
+
 
 def foo(s):
     return 10 / int(s)
@@ -88,6 +80,36 @@ def demo07():
     except Exception as e:
         logging.exception(e)
 
-demo07()
-print('END') # 还是会被执行
+# ------------------------------------------
+
+# 调试
+
+# 1. 使用断言代替 print()
+# 虽然断言比print也好不到哪去，但是python解释器可以通过 -O 参数去掉
+def tmp1(s):
+    n = int(s)
+    assert n != 0, 'n is not 0'
+    print('after')
+
+# 2. logging
+# 要注意当我们使用日志级别的时候，我们需要给日志配置级别
+def tmp2(s):
+    n = int(s)
+    logging.info('n = %d' % n)
+    print('after')
+
+if __name__ == '__main__':
+    # demo1()
+    # demo2()
+    # demo2_1()
+    # demo3()
+    # demo4()
+    # demo5()
+    # demo6(0)
+    # demo07()
+    # print('END')  # 还是还会被执行
+
+    # tmp1('0')
+    tmp2('0')
+
 
