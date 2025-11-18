1e6df869f5679b6fda8bc2c13df5e174ed6f4aa3
chasoneil <chason001@126.com>
2025-11-18 15:43:21 +0800

    review python base

--- src/_01_printAndInput/dataTypeAndLogic.py
+++ src/_01_printAndInput/dataTypeAndLogic.py
@@ -4,54 +4,67 @@
 # 数据类型和逻辑运算符
 # python的数据类型： 整数 浮点数 字符串 布尔 空值
 
-x = 10  # 整数
-print('x =', x)
+# 整数 浮点数
+def demo1():
+    x = 10  # 整数
+    print('x =', x)
 
-pi = 3.14 # 浮点数
-print('pi = ', pi)
+    pi = 3.14 # 浮点数
+    print('pi = ', pi)
 
-# python 中使用 '' / "" / ''' '''/ 来表示字符串
-print('single str')
-print("double str")
-# 字符串分行
-print('''long
-str''')
+# 字符串
+def demo2():
+    # python 中使用 '' / "" / ''' '''/ 来表示字符串
+    print('single str')
+    print("double str")
+    # 字符串分行
+    print('''long
+    str''')
 
-print('I\'m Chinese')  # 有些字符需要转义
-print(r'\\\\t1')  # 使用 r可以让转义失效
+    print('I\'m Chinese')  # 有些字符需要转义
+    print(r'\\\\t1')  # 使用 r可以让转义失效
 
-# python 中的布尔值是 True False
-if True:
-    print(True)
-else:
-    print(False)
+# 布尔和空值
+def demo3(flag):
+    # python 中的布尔值是 True False
+    if flag:
+        print(True)
+    else:
+        print(False)
 
-# python中使用None表示空值
-a = None
-print('a =', a)
+    # python中使用None表示空值
+    a = None
+    print('a =', a)
 
 # 2. python 中使用and or not来代表与或非
-print('使用and or not 做逻辑运算:')
-if True and True:
-    print('double true')
+def demo4(flag):
+    if True and True:
+        print('double true')
+    if True or False:
+        print('has true')
 
-if True or False:
-    print('has true')
+    if not flag:
+        print('flag is False')
+    else:
+        print('flag is True')
 
-if not True:
-    print('not true')
-else:
-    print('not false')
+# 使用 & | 做逻辑运算
+def demo5():
+    condition = True & True
+    if condition:
+        print('double true')
+    else:
+        print('has false')
 
-print('使用非关键字的方式做逻辑运算:')
-condition = True & True
-if condition:
-    print('double true')
-else:
-    print('has false')
+    condition = True | False
+    if  condition:
+        print('has true')
+    else:
+        print('double false')
 
-condition = True | False
-if  condition:
-    print('has true')
-else:
-    print('double false')
+if __name__ == '__main__':
+    demo1()
+    demo2()
+    demo3(False)
+    demo4(True)
+    demo5()
--- src/_01_printAndInput/start.py
+++ src/_01_printAndInput/start.py
@@ -1,37 +1,33 @@
 # 运行第一个python程序
 # python 支持交互式编程
 
-print("Hello python!")
-print("你好，Python") # 注意中文的编码
-
-print("01-----------------------")
+def start01():
+    print("Hello python!")
+    print("你好，Python") # 注意中文的编码
 
 # python可以在一行中写多个语句，语句之间用;隔开
-print("Hello");print("Python")
+def start02():
+    print("Hello");print("Python")
 
-print("02-----------------------")
-
-#python是一个很讲究缩进的语言
-if True:
-    print("True")
-else:
-    print("False")
-  #print("haha")  如果缩进不对，编译器会提示， 运行会弹出 IndentationError
-
-print("03-----------------------")
+# python是一个很讲究缩进的语言
+# 如果缩进不对，编译器会提示， 运行会弹出 IndentationError
+def start03(flag):
+    if flag:
+        print("True")
+    else:
+        print("False")
 
 #python的拼接和C类似 都是使用 \ 进行行的拼接
+def start04():
+    item_one = 1
+    item_two = 2
+    item_three = 3
 
-item_one = 1
-item_two = 2
-item_three = 3
+    total = item_one + \
+            item_two + \
+            item_three
 
-total = item_one + \
-        item_two + \
-        item_three
-
-print(total)
-print("04-----------------------")
+    print(total)
 
 # python 中 单引号，双引号 三引号都可以表示字符串 ，三个双引号还能表示多行注释
 
@@ -43,15 +39,22 @@ print("04-----------------------")
 多行注释方法2
 """
 
-word1 = 'p1'
-word2 = "p2"
-word3 = """p3
-p4"""
+def start05():
+    word1 = 'p1'
+    word2 = "p2"
+    word3 = """p3
+    p4"""
 
-print(word1)
-print(word2)
-print(word3)
-print("05-----------------------")
+    print(word1)
+    print(word2)
+    print(word3)
+
+if __name__ == '__main__':
+    start01()
+    start02()
+    start03(True)
+    start04()
+    start05()
 
 
 
